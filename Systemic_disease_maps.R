03f9bd6196aefb2aeae0c4300e0ef99f28b2c6e4
Michael Noel <michael-noel@users.noreply.github.com>
2014-11-20 10:35:11 +0800

    Update Systemic_disease_maps.R
    renamed file output to "Injury_Graphs/..."

--- Systemic_disease_maps.R
+++ Systemic_disease_maps.R
@@ -35,7 +35,7 @@ map + geom_point(data = bbn.summary, aes(x = lon, y = lat, size = rating, colour
   ggtitle("Bug Burn") +
   coord_map() +
   facet_grid(. ~ visit)
-ggsave("Graphs/Bug_burn_map.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Bug_burn_map.png", width = 8, height = 8, units = "in")
 
 # Hopper burn
 map + geom_point(data = hbn.summary, aes(x = lon, y = lat, size = rating, colour = rating)) + 
@@ -47,7 +47,7 @@ map + geom_point(data = hbn.summary, aes(x = lon, y = lat, size = rating, colour
   ggtitle("Hopper Burn") +
   coord_map() +
   facet_grid(. ~ visit)
-ggsave("Graphs/Hopper_burn_map.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Hopper_burn_map.png", width = 8, height = 8, units = "in")
 
 # Tungro
 map + geom_point(data = tun.summary, aes(x = lon, y = lat, size = rating, colour = rating)) + 
@@ -59,7 +59,7 @@ map + geom_point(data = tun.summary, aes(x = lon, y = lat, size = rating, colour
   ggtitle("Tungro") +
   coord_map() +
   facet_grid(. ~ visit)
-ggsave("Graphs/Tungro_map.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Tungro_map.png", width = 8, height = 8, units = "in")
 
 # Grassy stunt
 map + geom_point(data = grs.summary, aes(x = lon, y = lat, size = rating, colour = rating)) + 
@@ -71,7 +71,7 @@ map + geom_point(data = grs.summary, aes(x = lon, y = lat, size = rating, colour
   ggtitle("Grassy Stunt Virus") +
   coord_map() +
   facet_grid(. ~ visit)
-ggsave("Graphs/Grassy_stunt_map.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Grassy_stunt_map.png", width = 8, height = 8, units = "in")
 
 # Ragged Stunt
 map + geom_point(data = rgd.summary, aes(x = lon, y = lat, size = rating, colour = rating)) + 
@@ -83,7 +83,7 @@ map + geom_point(data = rgd.summary, aes(x = lon, y = lat, size = rating, colour
   ggtitle("Ragged Stunt Virus") +
   coord_map() +
   facet_grid(. ~ visit)
-ggsave("Graphs/Ragged_stunt_map.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Ragged_stunt_map.png", width = 8, height = 8, units = "in")
 
 # Orange leaf
 map + geom_point(data = olf.summary, aes(x = lon, y = lat, size = rating, colour = rating)) + 
@@ -95,7 +95,7 @@ map + geom_point(data = olf.summary, aes(x = lon, y = lat, size = rating, colour
   ggtitle("Orange Leaf") +
   coord_map() +
   facet_grid(. ~ visit)
-ggsave("Graphs/Orange_leaf_map.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Orange_leaf_map.png", width = 8, height = 8, units = "in")
 
 # Yellow dwarf
 map + geom_point(data = ylo.summary, aes(x = lon, y = lat, size = rating, colour = rating)) + 
@@ -107,7 +107,7 @@ map + geom_point(data = ylo.summary, aes(x = lon, y = lat, size = rating, colour
   ggtitle("Yellow Dwarf") +
   coord_map() +
   facet_grid(. ~ visit)
-ggsave("Graphs/Yellow_dwarf_map.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Yellow_dwarf_map.png", width = 8, height = 8, units = "in")
 
 #### End mapping ####
 
