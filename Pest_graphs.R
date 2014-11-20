dc10454cae34c00033ccf9465a48d362b55a4930
Michael Noel <michael-noel@users.noreply.github.com>
2014-11-20 09:57:14 +0800

    Update Pest_graphs.R
    rename file output names to "Injury_Graphs/..."

--- Pest_graphs.R
+++ Pest_graphs.R
@@ -25,7 +25,7 @@ ggplot(lfd, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Leaf Folder Damage")
-ggsave("Graphs/Leaf_folder.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Leaf_folder.png", width = 8, height = 8, units = "in")
 
 # Leaf miner
 ggplot(lfm, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -37,7 +37,7 @@ ggplot(lfm, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Leaf Miner Damage")
-ggsave("Graphs/Leaf_miner.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Leaf_miner.png", width = 8, height = 8, units = "in")
 
 # Thrip
 ggplot(thp, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -49,7 +49,7 @@ ggplot(thp, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Thrip Damage")
-ggsave("Graphs/Thrip.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Thrip.png", width = 8, height = 8, units = "in")
 
 # Whorl maggot
 ggplot(whm, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -61,7 +61,7 @@ ggplot(whm, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Whorl Maggot Damage")
-ggsave("Graphs/Whorl_maggot.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Whorl_maggot.png", width = 8, height = 8, units = "in")
 
 # Other defoliator
 ggplot(def, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -73,7 +73,7 @@ ggplot(def, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Other Defoliator Damage")
-ggsave("Graphs/Defoliator.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Defoliator.png", width = 8, height = 8, units = "in")
 
 # White head
 ggplot(wht.graph, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -85,7 +85,7 @@ ggplot(wht.graph, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("White Head Incidence")
-ggsave("Graphs/White_Head.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/White_Head.png", width = 8, height = 8, units = "in")
 
 # Rice grain bug
 ggplot(rgb.graph, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -97,7 +97,7 @@ ggplot(rgb.graph, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Rice Grain Bug Damage")
-ggsave("Graphs/Rice_Grain_Bug.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Rice_Grain_Bug.png", width = 8, height = 8, units = "in")
 
 # Rice bug
 ggplot(rbg, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -109,7 +109,7 @@ ggplot(rbg, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Rice Bug Damage")
-ggsave("Graphs/Rice_Bug.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Rice_Bug.png", width = 8, height = 8, units = "in")
 
 # Dead heart
 ggplot(dht, aes(x = factor(Municipality), y = (injury/organ)*100)) +
@@ -121,7 +121,7 @@ ggplot(dht, aes(x = factor(Municipality), y = (injury/organ)*100)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Dead Heart")
-ggsave("Graphs/Dead_Heart.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Dead_Heart.png", width = 8, height = 8, units = "in")
 
 # Rat damage during two visits
 ggplot(rat, aes(x = factor(Municipality), y = injury)) +
@@ -133,7 +133,7 @@ ggplot(rat, aes(x = factor(Municipality), y = injury)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   ggtitle("Rat Damage") +
   facet_grid(. ~ visit)
-ggsave("Graphs/Rat_Graph.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Rat_Graph.png", width = 8, height = 8, units = "in")
 
 # golden apple snail damage
 ggplot(gas, aes(x = factor(Municipality), y = injury)) +
@@ -144,7 +144,7 @@ ggplot(gas, aes(x = factor(Municipality), y = injury)) +
   scale_colour_discrete(name = "Region") +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   ggtitle("Golden Apple Snail Damage")
-ggsave("Graphs/GAS_Graph.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/GAS_Graph.png", width = 8, height = 8, units = "in")
 
 #### Progress curve graphs ####
 # Rat AUIPC
@@ -156,6 +156,6 @@ ggplot(rat.wide, aes(x = factor(Municipality), y = damage)) +
   scale_colour_discrete(name = "Region") +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   ggtitle("End of Season Summary\nRat Damage by Municipality")
-ggsave("Graphs/Rat_AUIPC_Graph.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Rat_AUIPC_Graph.png", width = 8, height = 8, units = "in")
 
 # eos
