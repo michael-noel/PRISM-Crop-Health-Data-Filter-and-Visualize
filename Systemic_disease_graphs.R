4e2f6717b56503aa6e6ec6857ac12aca6fa8896a
Michael Noel <michael-noel@users.noreply.github.com>
2014-11-20 10:36:23 +0800

    Update Systemic_disease_graphs.R
    renamed file output to "Injury_Graphs/..."

--- Systemic_disease_graphs.R
+++ Systemic_disease_graphs.R
@@ -25,7 +25,7 @@ ggplot(bbn, aes(x = factor(Municipality), y = rating)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Bug Burn Severity")
-ggsave("Graphs/Bug_burn.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Bug_burn.png", width = 8, height = 8, units = "in")
 
 # hopper burn
 ggplot(hbn, aes(x = factor(Municipality), y = rating)) +
@@ -37,7 +37,7 @@ ggplot(hbn, aes(x = factor(Municipality), y = rating)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Hopper Burn Severity")
-ggsave("Graphs/Hopper_burn.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Hopper_burn.png", width = 8, height = 8, units = "in")
 
 # tungro damage
 ggplot(tun, aes(x = factor(Municipality), y = rating)) +
@@ -49,7 +49,7 @@ ggplot(tun, aes(x = factor(Municipality), y = rating)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Tungro Area")
-ggsave("Graphs/Tungro.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Tungro.png", width = 8, height = 8, units = "in")
 
 # grassy stunt damage
 ggplot(grs, aes(x = factor(Municipality), y = rating)) +
@@ -61,7 +61,7 @@ ggplot(grs, aes(x = factor(Municipality), y = rating)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Grassy Stunt Area")
-ggsave("Graphs/Grassy_stunt.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Grassy_stunt.png", width = 8, height = 8, units = "in")
 
 # grassy stunt damage
 ggplot(rgd, aes(x = factor(Municipality), y = rating)) +
@@ -73,7 +73,7 @@ ggplot(rgd, aes(x = factor(Municipality), y = rating)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Rice Ragged Stunt Area")
-ggsave("Graphs/Rice_ragged_stunt.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Rice_ragged_stunt.png", width = 8, height = 8, units = "in")
 
 # grassy stunt damage
 ggplot(olf, aes(x = factor(Municipality), y = rating)) +
@@ -85,7 +85,7 @@ ggplot(olf, aes(x = factor(Municipality), y = rating)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Orange Leaf Area Area")
-ggsave("Graphs/Orange_leaf.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Orange_leaf.png", width = 8, height = 8, units = "in")
 
 ggplot(ylo, aes(x = factor(Municipality), y = rating)) +
   geom_boxplot(aes(colour = factor(Region), fill = factor(Region)), alpha = 0.65, outlier.colour = "darkred", outlier.size = 4) +
@@ -96,6 +96,6 @@ ggplot(ylo, aes(x = factor(Municipality), y = rating)) +
   theme(axis.text.x = element_text(angle = 35, hjust = 0.8)) +
   facet_grid(. ~ visit) +
   ggtitle("Yellow Dwarf Area Area")
-ggsave("Graphs/Yellow_dwarf.png", width = 8, height = 8, units = "in")
+ggsave("Injury_Graphs/Yellow_dwarf.png", width = 8, height = 8, units = "in")
 
 # eos
